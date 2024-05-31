342c94db4fe3bd03d72ae6c954cc6364ce253ef9
zarnigordekhonova <zarnigor1008@gmail.com>
2024-05-31 12:18:36 +0500

    zarnigor

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from users.models import CustomUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+    class Meta:
+        db_table = 'category'
+
+    def __str__(self):
+        return self.name
+
+class Articles(models.Model):
+    title = models.CharField(max_length=256, blank=True, null=True)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    text = models.TextField(blank=True, null=True)
+    star_given = models.IntegerField(
+        default=0,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(5)
+        ]
+    )
+    image = models.ImageField(upload_to='article_images/', blank=True, null=True, default='default_images/not_available.png')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'articles'
+
+    def __str__(self):
+        return f'{self.category.name} - {self.title}'
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Category, Articles
+from django.views import View
 # Create your views here.
+
+
