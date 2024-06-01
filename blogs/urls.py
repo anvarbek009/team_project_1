50552702e8a24f3ba0e410a0f96158623de622e3
zarnigordekhonova <zarnigor1008@gmail.com>
2024-06-01 17:44:29 +0500

    zarnigor

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CategoryListView
+from .views import CategoryListView, ArticlesListView, ArticlesUpdateView, ArticlesDetailView
 
 app_name = 'blogs'
 urlpatterns = [
-    path('category-list', CategoryListView.as_view(), name='category-list')
+    path('category-list', CategoryListView.as_view(), name='category-list'),
+    path('articles-list/<int:pk>/', ArticlesListView.as_view(), name='articles-list'),
+    path('articles-detail/<int:pk>/', ArticlesDetailView.as_view(), name='articles-detail'),
+    path('articles/<int:pk>/update/', ArticlesUpdateView.as_view(), name='articles-update')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Category, Articles
 from django.views import View
+from django.views.generic.edit import UpdateView, CreateView
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -11,4 +13,29 @@ class CategoryListView(View):
             'category' : category
         }
         return render(request, 'blogs/category_list.html', context=context)
+
+class ArticlesListView(View):
+    def get(self, request, pk):
+        articles = Articles.objects.filter(category=pk)
+        context = {
+            'articles' : articles
+        }
+        return render(request, 'blogs/articles_list.html', context=context)
+
+
+class ArticlesDetailView(View):
+    def get(self, request, pk):
+        article = Articles.objects.get(pk=pk)
+        return render(request, 'blogs/articles_detail.html', {'article' : article})
+
+
+class ArticlesUpdateView(UpdateView):
+    model = Articles
+    template_name = 'blogs/articles_update.html'
+    fields = ['title', 'text']
+
+    def get_success_url(self):
+        return reverse_lazy('blogs:articles-detail', kwargs={'pk': self.object.pk})
+
+
 
