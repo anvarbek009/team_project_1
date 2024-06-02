3d7ddceb9be64a9063b713f2d5e78dce90852ae1
anvarbek009 <anvarasqarov2009@gmail.com>
2024-06-02 20:51:55 +0500

    Anvarbek

--- conf/settings.py
+++ conf/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'static'
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'home:home_page'
 LOGOUT_REDIRECT_URL = 'home:home_page'
